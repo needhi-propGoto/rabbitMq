71877065f23fe4411bc5c9bd79b90a51155ad70b
needhi-propGoto <m.needhichozhan@propertyautomate.com>
2025-01-13 12:53:22 +0530

    changes in producer & consumer func

--- consumer.js
+++ consumer.js
@@ -1,23 +1,19 @@
 const amqp = require("amqplib");
 
-
 const connect = async () => {
+  try {
+    const connection = await amqp.connect("amqp://localhost:5672");
+    const channel = await connection.createChannel();
+    const result = await channel.assertQueue("jobs");
 
-    try{
-        const connection = await amqp.connect("amqp://localhost:5672");
-        const channel = await connection.createChannel();
-        const result = await channel.assertQueue("jobs");
+    channel.consume("jobs", (message) => {
+      console.log(`Received job with content ${message.content.toString()}`);
+    });
 
-        channel.consume("jobs", message => {
-            console.log(`Received job with content ${message.content.toString()}`);
-        })
-
-        console.log("Waiting for messages...");
-
-    } catch (error) {
-        console.error(error);
-    }
-    
+    console.log("Waiting for messages...");
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 connect();
--- producer.js
+++ producer.js
@@ -3,17 +3,17 @@ const amqp = require("amqplib");
 const msg = { number: process.argv[2] };
 
 const connect = async () => {
-    try{
-        const connection = await amqp.connect("amqp://localhost:5672");
-        const channel = await connection.createChannel();
-        const result = await channel.assertQueue("jobs");
-        channel.sendToQueue("jobs", Buffer.from(JSON.stringify(msg)));
-        console.log(`Job sent successfully ${msg.number}`);
-        await channel.close();
-    } catch (error) {
-        console.error(error);
-    }
+  try {
+    const connection = await amqp.connect("amqp://localhost:5672");
+    const channel = await connection.createChannel();
+    const result = await channel.assertQueue("jobs");
+
+    channel.sendToQueue("jobs", Buffer.from(JSON.stringify(msg)));
+    console.log(`Job sent successfully ${msg.number}`);
+
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 connect();
-
