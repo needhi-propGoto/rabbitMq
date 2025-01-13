2e8f4ab4c82a899156079f6eab88c6ca730a7e00
needhi-propGoto <m.needhichozhan@propertyautomate.com>
2025-01-13 12:51:08 +0530

    init commit

---
+++ consumer.js
@@ -0,0 +1,23 @@
+const amqp = require("amqplib");
+
+
+const connect = async () => {
+
+    try{
+        const connection = await amqp.connect("amqp://localhost:5672");
+        const channel = await connection.createChannel();
+        const result = await channel.assertQueue("jobs");
+
+        channel.consume("jobs", message => {
+            console.log(`Received job with content ${message.content.toString()}`);
+        })
+
+        console.log("Waiting for messages...");
+
+    } catch (error) {
+        console.error(error);
+    }
+    
+};
+
+connect();
---
+++ producer.js
@@ -0,0 +1,19 @@
+const amqp = require("amqplib");
+
+const msg = { number: process.argv[2] };
+
+const connect = async () => {
+    try{
+        const connection = await amqp.connect("amqp://localhost:5672");
+        const channel = await connection.createChannel();
+        const result = await channel.assertQueue("jobs");
+        channel.sendToQueue("jobs", Buffer.from(JSON.stringify(msg)));
+        console.log(`Job sent successfully ${msg.number}`);
+        await channel.close();
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+connect();
+
